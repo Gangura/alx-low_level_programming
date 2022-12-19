e2bf97f22dc85beffa771a47e41857de8c7beed0
“Gangura” <gabrielangura@gmail.com>
2022-12-19 00:57:09 -0800

    All nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ ** main - Print the word "_putchar"
+ **
+ ** Return: Always 0 (Success)
+ **/
+
+int main(void)
+{
+	char p[] = "_putchar";
+	int i;
+
+	for (i = 0; ch [i] != '\0'; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

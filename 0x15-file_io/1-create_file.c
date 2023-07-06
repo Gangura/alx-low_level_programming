6eb676677cd0919e6b0e1409c8b45c2ad8233197
GABRIEL ANGURA <61846218+Gangura@users.noreply.github.com>
2023-07-06 18:19:55 +0300

    Files

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * create_file - function with two arguments
+ * @filename: pointer to string name
+ * @text_content: pointer to string contents
+ *
+ * Description: creates a file
+ * Return: 1 on success or -1 failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int count = 0;
+	int fd = 0;
+	int output = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[count] != '\0')
+			count++;
+	}
+	else
+	{
+		close(fd);
+		return (1);
+	}
+
+	output = write(fd, text_content, count);
+	if (output == -1 || output != count)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
